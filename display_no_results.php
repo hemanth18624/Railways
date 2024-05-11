6ba35b82c4795ab40e67d9ff103d792e470bdd3f
Hemanth <123809089+hemanth18624@users.noreply.github.com>
2024-05-12 00:01:08 +0530

    Create display_no_results.php

---
+++ display_no_results.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+    <link rel = "stylesheet" href = "display_no_results.css">
+</head>
+<body>
+    <div class="display">
+        <h1>No Trains Found</h1>
+    </div>
+</html>
