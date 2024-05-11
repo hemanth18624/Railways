fdeb1b386c827b969d26c2865b36a257394fc3c1
Hemanth <123809089+hemanth18624@users.noreply.github.com>
2024-05-12 00:07:08 +0530

    Create payment_processing.php

---
+++ payment_processing.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href = "payment_processing.css">
+    <title>Payment Processing</title>
+</head>
+<body>
+    <div class="center">
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+    </div>
+    <div class="display">
+        <h1>Payment Processing.....Please Wait</h1>
+    </div>
+    <script>
+        setTimeout(function() {
+          window.location.href = "index.html";
+        }, 5000); // 5000 milliseconds = 5 seconds
+      </script>
+</body>
+</html>
